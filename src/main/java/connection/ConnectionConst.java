b411b8bc74a55b36c8743dd824c99e3ab38d1ff5
djawlgns7 <djawlgns7@gmail.com>
2024-04-01 20:16:33 +0900

    test

--- src/main/java/connection/ConnectionConst.java
+++ src/main/java/connection/ConnectionConst.java
@@ -5,5 +5,4 @@ public abstract class ConnectionConst {
     public static final String URL = "jdbc:mysql://localhost:3306/testgym";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "1030";
-    public static final String test = "asdf";
 }
