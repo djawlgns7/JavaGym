29c99312d64b40a90231765a9a38e6eaace9a86f
Noh Seung Been <shtmdqls990921@gmail.com>
2024-04-08 14:31:34 +0900

    MemberInfo 페이지 수정

--- src/main/java/JavaGymApplication.java
+++ src/main/java/JavaGymApplication.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -11,9 +13,9 @@ public class JavaGymApplication extends Application {
     public void start(Stage stage) throws Exception {
         // 폰트 로드
         Font.loadFont(getClass().getResourceAsStream("/fonts/Anton-Regular.ttf"), 20);
-        Font.loadFont(getClass().getResourceAsStream("/fonts/BlackHanSans-Regular.ttf"), 20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/Jua-Regular.ttf"), 20);
 
-        Parent loginRoot = FXMLLoader.load(getClass().getResource("/view/member/memberLogin.fxml"));
+        Parent loginRoot = FXMLLoader.load(getClass().getResource("/view/admin/memberInfo.fxml"));
         stage.setScene(new Scene(loginRoot));
         stage.setTitle("JavaGym");
         stage.show();
