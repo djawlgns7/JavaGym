ea40f3156fb72a4f62e9a98bf87e32d02792deef
Noh Seung Been <shtmdqls990921@gmail.com>
2024-04-10 21:56:53 +0900

    css/password 삭제

--- src/main/java/controller/member/HelloMemberController.java
+++ src/main/java/controller/member/HelloMemberController.java
@@ -32,11 +32,9 @@ public class HelloMemberController implements Initializable {
 
     @FXML
     private void myInfo(ActionEvent event) throws IOException {
-        movePage(event, "/view/member/myInformation", "/css/password");
+        movePage(event, "/view/member/myInformation");
     }
 
-    // 바로입장 버튼에 대한 메소드 추가
-    // @@@ 바로입장 버튼을 눌렀을 때 문이 열리는 기능 추가 할 것 @@@
     @FXML
     public void entry(ActionEvent event) throws IOException {
         showAlertAndMove(currentMember.getName() + "님 오늘도 파이팅!", Alert.AlertType.INFORMATION, "/view/member/memberLogin", event);
