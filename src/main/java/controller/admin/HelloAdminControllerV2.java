268adf6f04e87902ef882e5763b06b1880c96f17
ohmintaeak <omt610@gmail.com>
2024-04-14 17:48:26 +0900

    예약 페이지 공사 완료

--- src/main/java/controller/admin/HelloAdminControllerV2.java
+++ src/main/java/controller/admin/HelloAdminControllerV2.java
@@ -85,7 +85,7 @@ public class HelloAdminControllerV2 implements Initializable {
         member.setPhone(phone);
 
         service.addMember(member);
-        showAlertAndMove("회원 등록 성공", Alert.AlertType.INFORMATION, "/view/admin/helloAdmin2", event);
+        showAlertAndMove("회원 등록 성공", Alert.AlertType.INFORMATION, "/view/admin/helloAdminV2", event);
     }
 
     @FXML
@@ -320,7 +320,7 @@ public class HelloAdminControllerV2 implements Initializable {
             }
         }
 
-        showAlertAndMove("트레이너 등록 성공", Alert.AlertType.INFORMATION, "/view/admin/helloAdmin2", event);
+        showAlertAndMove("트레이너 등록 성공", Alert.AlertType.INFORMATION, "/view/admin/helloAdminV2", event);
     }
 
     @FXML
