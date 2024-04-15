b3be185953a09932fa94b83e1d87371ce4dd79dc
Noh Seung Been <shtmdqls990921@gmail.com>
2024-04-15 21:53:40 +0900

    04/15 21:53 1. 결제 페이지 디자인 (진행 중) 2. 유지 보수

--- src/main/java/JavaGymApplication.java
+++ src/main/java/JavaGymApplication.java
@@ -5,7 +5,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 import repository.MemberRepository;
+
 
 public class JavaGymApplication extends Application {
 
@@ -29,10 +31,17 @@ public class JavaGymApplication extends Application {
         // 폰트 로드
         Font.loadFont(getClass().getResourceAsStream("/fonts/Anton-Regular.ttf"), 20);
         Font.loadFont(getClass().getResourceAsStream("/fonts/Jua-Regular.ttf"), 20);
+        Font.loadFont(getClass().getResourceAsStream("/fonts/NanumGothic-Regular.ttf"), 20);
+
         SelectedMember.currentMember = repository.findByNum(1068);
 
         Parent loginRoot = FXMLLoader.load(getClass().getResource("/view/member/memberLogin.fxml"));
         stage.setScene(new Scene(loginRoot));
+
+        // 프로젝트 실행 아이콘 이미지 로드 (승빈)
+        Image icon = new Image(getClass().getResourceAsStream("/image/JavaGym_Logo.jpeg"));
+        stage.getIcons().add(icon);
+
         stage.setTitle("JavaGym");
         stage.show();
         stage.setResizable(false);
