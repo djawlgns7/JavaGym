15f0b8d149cf2d79046711eb850d1876d70cd9a0
ohmintaeak <omt610@gmail.com>
2024-04-16 12:52:53 +0900

    내정보.css 썩 물러가라

--- src/main/java/thread/InactivityManager.java
+++ src/main/java/thread/InactivityManager.java
@@ -67,8 +67,8 @@ public class InactivityManager {
         }
 
         KeyFrame alertFrame = new KeyFrame(Duration.seconds(10), e -> SoundUtil.play("toMainPage"));
-        KeyFrame DialogFrame = new KeyFrame(Duration.seconds(10), e -> openTimerDialog());
-        KeyFrame endFrame = new KeyFrame(Duration.seconds(20), e -> moveToMainScreen());
+        KeyFrame DialogFrame = new KeyFrame(Duration.seconds(5), e -> openTimerDialog());
+        KeyFrame endFrame = new KeyFrame(Duration.seconds(30), e -> moveToMainScreen());
 
         inactivityTimer = new Timeline(alertFrame, endFrame, DialogFrame);
         inactivityTimer.setCycleCount(Timeline.INDEFINITE);
