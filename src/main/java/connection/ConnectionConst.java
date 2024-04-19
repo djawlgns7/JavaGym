4c8c172490a9e39783a8122098b715ffe6e24886
“serimmmaime” <yesellya@gmail.com>
2024-04-19 17:37:37 +0900

    checkout serim

--- src/main/java/connection/ConnectionConst.java
+++ src/main/java/connection/ConnectionConst.java
@@ -4,5 +4,5 @@ public abstract class ConnectionConst {
 
     public static final String URL = "jdbc:mysql://localhost:3306/javagym";
     public static final String USERNAME = "root";
-    public static final String PASSWORD = "1923";
+    public static final String PASSWORD = "wafers333";
 }
